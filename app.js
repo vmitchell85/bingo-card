4235d35f94f3fa85a669a7d8f20876f22dd7c62c
Vince Mitchell <vincent.mitchell@gmail.com>
2020-04-02 23:00:37 -0400

    wip

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var letterMap = {
+const letterMap = {
     B: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     I: [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
     N: [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
@@ -6,16 +6,20 @@ var letterMap = {
     O: [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]
 };
 
-['B', 'I', 'N', 'G', 'O'].forEach(letter => {
-    var numbers = letterMap[letter];
+function loadCard() {
+    ['B', 'I', 'N', 'G', 'O'].forEach(letter => {
+        let numbers = JSON.parse(JSON.stringify(letterMap[letter]));
 
-    for (let x = 1; x <= 5; x++) {
-        var currentID = letter + x;
-        if (currentID !== 'N3') {
-            var number = numbers[Math.floor(Math.random() * numbers.length)];
-            var index = numbers.indexOf(number);
-            numbers.splice(index, 1);
-            document.getElementById(currentID).innerHTML = number;
+        for (let x = 1; x <= 5; x++) {
+            var currentID = letter + x;
+            if (currentID !== 'N3') {
+                var number = numbers[Math.floor(Math.random() * numbers.length)];
+                var index = numbers.indexOf(number);
+                numbers.splice(index, 1);
+                document.getElementById(currentID).innerHTML = number;
+            }
         }
-    }
-});
+    });
+}
+
+loadCard();
